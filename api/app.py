6763223a99a9f0911a636b05e709ccf757d47173
RISHAB <rishimasana@gmail.com>
2026-05-21 08:50:45 +0100

    DAY 5

--- api/app.py
+++ api/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+import logging
+
+# Configure logging
+logging.basicConfig(
+    filename='logs/app.log',
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s'
+)
 
 # Create Flask app
 app = Flask(__name__)
@@ -8,7 +16,7 @@ app = Flask(__name__)
 # Load trained model
 model = joblib.load("models/cricket_score_model.pkl")
 
-print("MODEL LOADED SUCCESSFULLY")
+logging.info("MODEL LOADED SUCCESSFULLY")
 
 # Home route
 @app.route("/")
@@ -19,20 +27,45 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
 
-    # Get JSON data
-    data = request.get_json()
+    try:
 
-    # Convert input into DataFrame
-    input_data = pd.DataFrame([data])
+        # Receive JSON input
+        data = request.get_json()
 
-    # Make prediction
-    prediction = model.predict(input_data)
+        logging.info(f"Incoming data: {data}")
 
-    # Return response
-    return jsonify({
-        "Predicted Score": float(prediction[0])
-    })
+        # Validation
+       	required_features = ["Match ID","Overs Played","Wickets Lost","Run Rate","Home/Away",    "Opponent Strength",    "Pitch Condition", "Weather"]
 
-# Run Flask app
+        for feature in required_features:
+            if feature not in data:
+                logging.error(f"Missing feature: {feature}")
+
+                return jsonify({
+                    "error": f"Missing feature: {feature}"
+                })
+
+        # Convert to DataFrame
+        input_data = pd.DataFrame([data])
+
+        # Prediction
+        prediction = model.predict(input_data)
+
+        logging.info(f"Prediction successful: {prediction[0]}")
+
+        # Return prediction
+        return jsonify({
+            "Predicted Score": float(prediction[0])
+        })
+
+    except Exception as e:
+
+        logging.error(f"Error occurred: {str(e)}")
+
+        return jsonify({
+            "error": str(e)
+        })
+
+# Run app
 if __name__ == "__main__":
     app.run(debug=True)
